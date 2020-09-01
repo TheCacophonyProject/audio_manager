1bf8f413aac578ccf562c4d3c1bf525aaf1db49e
timhot <timhot@hotmail.com>
2020-09-01 17:49:38 +1200

    more on confusion matrix

---
+++ audio_manager/src/prepare_data_v4.py
@@ -0,0 +1,254 @@
+'''
+Created on 1 Sep. 2020
+
+@author: tim
+'''
+
+import functions
+import parameters
+import librosa
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.utils import to_categorical 
+
+
+BASE_FOLDER = '/home/tim/Work/Cacophony'
+MODEL_RUN_NAME = "2020_08_24a"
+RUNS_FOLDER = '/Audio_Analysis/audio_classifier_runs/tensorflow_runs/' 
+
+   
+         
+# def what_to_integer_lookup(what): # https://stackoverflow.com/questions/60208/replacements-for-switch-statement-in-python
+#     return {
+#         'bird': 1,
+#         'buzzy_insect': 2,
+#         'car': 3,
+#         'car_horn': 4,
+#         'chainsaw': 5,
+#         'cow': 6,
+#         'crackle': 7,
+#         'dog': 8,
+#         'dove': 9,
+#         'duck': 10,
+#         'fire_work': 11,
+#         'frog': 12,
+#         'hammering': 13,
+#         'hand_saw': 14,
+#         'human': 15,
+#         'maybe_morepork_more-pork': 16,
+#         'morepork_more-pork': 17,
+#         'morepork_more-pork_part': 18,
+#         'music': 19,
+#         'plane': 20,
+#         'rumble': 21,
+#         'siren': 22,
+#         'unknown': 23,
+#         'water': 24,
+#         'white_noise': 25         
+#     }.get(what, 0)    # 0 is default if what not found
+
+def create_integer_to_sound_mapping(sound_to_integer_mapping):
+
+    print(sound_to_integer_mapping)
+    integer_to_sound_mapping = {}
+    for key, value in sound_to_integer_mapping.items():
+#         print(item)
+        integer_to_sound_mapping[value] = key
+        
+    print(integer_to_sound_mapping)
+    return integer_to_sound_mapping
+
+def one_hot_encode_labels(sounds):
+    # https://www.educative.io/edpresso/how-to-perform-one-hot-encoding-using-keras
+    total_sounds = np.unique(sounds)
+    print("len(total_sounds) ", len(total_sounds))
+
+    # map each sound to an integer
+    sound_to_integer_mapping = {}
+    for x in range(len(total_sounds)):
+        sound_to_integer_mapping[total_sounds[x]] = x
+    
+    # integer representation
+    for x in range(len(sounds)):
+        sounds[x] = sound_to_integer_mapping[sounds[x]]
+    
+    one_hot_encode = to_categorical(sounds)  
+    integer_to_sound_mapping = create_integer_to_sound_mapping(sound_to_integer_mapping)
+#     return one_hot_encode, mapping
+    return one_hot_encode, integer_to_sound_mapping          
+
+def get_onsets_stored_locally_for_recording_id(version_to_use, recording_id):
+    cur = functions.get_database_connection().cursor()    
+    cur.execute("SELECT start_time_seconds, duration_seconds, actual_confirmed FROM onsets WHERE version = ? AND recording_id = ? ORDER BY recording_id", (version_to_use, recording_id)) 
+    rows = cur.fetchall()
+    return rows
+
+def get_filtered_recording_for_onset(recording_id, start_time):
+    recordings_folder_with_path = parameters.base_folder + '/' + parameters.downloaded_recordings_folder
+    filename = str(recording_id) + ".m4a"
+    audio_in_path = recordings_folder_with_path + "/" + filename
+
+    y, sr = librosa.load(audio_in_path, sr=22050, mono=True, offset=start_time, duration=0.7314) # chosen to give a square with the 32 mels
+        
+    y_filtered = functions.butter_bandpass_filter(y, parameters.morepork_min_freq, parameters.morepork_max_freq, sr)    
+    
+    return y_filtered, sr
+
+
+
+
+def load_onset_audio(recording_id, start_time):
+   
+  
+    y, sr = get_filtered_recording_for_onset(recording_id, start_time)
+    mfccs = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=32, fmin=700,fmax=1000, hop_length=512) # https://librosa.org/doc/latest/generated/librosa.feature.melspectrogram.html
+    
+    max_value = np.max(mfccs)    
+
+    mfccs_normalized = mfccs / max_value
+    
+    # As we are going to use a Conv2d layer in the model, it expects 3 dimensions, so need to expand
+#     https://machinelearningmastery.com/a-gentle-introduction-to-channels-first-and-channels-last-image-formats-for-deep-learning/
+#     print("mfccs_normalized.shape ", mfccs_normalized.shape)    
+    mfccs_normalized = np.expand_dims(mfccs_normalized, axis=2)    
+    print("mfccs_normalized.shape ", mfccs_normalized.shape)
+   
+#     if mfccs_normalized.shape[1] < 2584:   # need to change this to ? 
+    if mfccs_normalized.shape[1] < 32:   # need to change this to ? 
+
+#         reshaped_mfccs = np.zeros((32, 2584, 1))
+#         reshaped_mfccs[:mfccs_normalized.shape[0],:mfccs_normalized.shape[1]] = mfccs_normalized
+#        
+# #         print(reshaped_mfccs.shape)
+#         return reshaped_mfccs 
+        return None # just throw it away
+       
+    else:
+        return mfccs_normalized  
+
+
+def get_all_training_onset_data(testing, display_image):
+    
+    version_to_use = 5
+    cur = functions.get_database_connection().cursor()    
+    cur.execute("select recording_id, start_time_seconds, actual_confirmed FROM onsets WHERE version = ? AND actual_confirmed IS NOT NULL ORDER BY recording_id", (version_to_use, )) 
+    onsets = cur.fetchall()
+    
+    number_of_onsets = len(onsets)
+    if testing:
+        number_of_onsets = 100
+    
+    # https://stackoverflow.com/questions/53135673/how-to-use-numpy-dstack-in-a-loop
+    array_of_mfccs = []
+    array_of_labels = []
+
+    count = 0
+    for onset in onsets:
+        count+=1
+        print("Processing ", count, " of ", number_of_onsets)
+        recording_id = onset[0]
+        start_time = onset[1]
+        actual_confirmed = onset[2]
+        
+        if actual_confirmed == 'maybe_morepork_more-pork' or actual_confirmed == 'morepork_more-pork_part':
+            continue # won't use them for training  
+        
+        
+        
+        mfccs = load_onset_audio(recording_id, start_time)
+        if mfccs is not None:
+
+            array_of_mfccs.append(mfccs)
+
+            array_of_labels.append(actual_confirmed)
+              
+        if testing:
+            if count >= number_of_onsets:
+                break
+            
+            
+        if display_image:
+            result = mfccs[:, :, 0]
+            print(result.shape)
+
+            plt.matshow(result)
+            plt.title(actual_confirmed)
+            plt.show()
+           
+ 
+    result_mfccs = np.stack(array_of_mfccs, axis=0)
+    result_labels = np.stack(array_of_labels, axis=0)
+    
+    print("result_mfccs shape is ", result_mfccs.shape)
+    print("result_labels shape is ", result_labels.shape)
+    
+   
+    
+    return result_mfccs, result_labels
+
+
+   
+def get_data(create_data, testing, display_image):
+    array_of_mfccs_filename = BASE_FOLDER + RUNS_FOLDER +  MODEL_RUN_NAME + "/" + 'array_of_mfccs' 
+    array_of_labels_filename = BASE_FOLDER + RUNS_FOLDER +  MODEL_RUN_NAME + "/" + 'array_of_labels'
+           
+    if create_data:
+        array_of_mfccs, array_of_labels = get_all_training_onset_data(testing=testing, display_image=display_image)    
+        np.save(array_of_mfccs_filename, array_of_mfccs)
+        np.save(array_of_labels_filename, array_of_labels)
+        
+    else:
+        # read from previously saved data
+        array_of_mfccs = np.load(array_of_mfccs_filename + ".npy")
+        array_of_labels = np.load(array_of_labels_filename + ".npy")
+        
+    number_of_distinct_labels = len(np.unique(array_of_labels))
+     
+    print("number_of_distinct_labels ", number_of_distinct_labels)
+        
+#     print("array_of_mfccs shape is ", array_of_mfccs.shape)
+#     print("array_of_labels shape is ", array_of_labels.shape)
+
+    # https://www.educative.io/edpresso/how-to-perform-one-hot-encoding-using-keras
+#     array_of_labels, sound_to_integer_mapping = one_hot_encode_labels(array_of_labels)
+    array_of_labels, integer_to_sound_mapping = one_hot_encode_labels(array_of_labels)
+    
+   
+
+    X_train, X_test, y_train, y_test = train_test_split(array_of_mfccs,
+                                                    array_of_labels,
+                                                    test_size=0.33,
+                                                    random_state=42)    
+    
+
+    
+#     return X_train, X_test, y_train, y_test, number_of_distinct_labels, sound_to_integer_mapping
+    return X_train, X_test, y_train, y_test, number_of_distinct_labels, integer_to_sound_mapping
+
+def run(create_data, testing, display_image):
+    print("Started")
+    
+#     X_train, X_test, y_train, y_test, number_of_distinct_labels, sound_to_integer_mapping = get_data(create_data=create_data, testing=testing, display_image=display_image)
+    X_train, X_test, y_train, y_test, number_of_distinct_labels, integer_to_sound_mapping = get_data(create_data=create_data, testing=testing, display_image=display_image)  
+     
+       
+
+    
+    print("number_of_distinct_labels", number_of_distinct_labels)
+    
+    print("sound_to_integer_mapping ", integer_to_sound_mapping)
+       
+    print("Finished")
+
+
+# run(True) # True means create data from recordings and database, False means read in already saved numpy files
+
+if __name__ == '__main__':
+    # create_data=True means create data from recordings and database, False means read in already saved numpy files
+    # testing=False means only create data for 3 recordings
+    run(create_data=False, testing=True, display_image=False) 
+
---
+++ audio_manager/src/what_v11.py
@@ -0,0 +1,269 @@
+'''
+Created on 1 Sept. 2020
+This will take the mel spectrums directly without going via pics
+It will not rely on onsets, but instead look at whole recording (or sliding window)
+Train in whole recordings with labels of has morepork / no morepork or maybe how many moreporks
+
+Use this to learn how to input data into Tensorflow from numpy arrays
+@author: tim
+'''
+
+import numpy as np
+import datetime
+import random
+import sys
+import os
+
+import time
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import regularizers
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import GlobalAveragePooling2D
+from tensorflow.keras.layers import Activation
+
+import io
+import itertools
+import sklearn.metrics
+from datetime import datetime
+# from tensorflow.summary import create_file_writer
+
+import prepare_data_v4
+
+BASE_FOLDER = '/home/tim/Work/Cacophony'
+RUNS_FOLDER = '/Audio_Analysis/audio_classifier_runs/tensorflow_runs/' 
+MODEL_RUN_NAME = "2020_09_01a"
+run_sub_log_dir = "4-testing"
+
+
+def create_model_basic(num_classes):
+    # 8-DO(0.8-0.2-0.2-0.2-0.2) 
+    model = Sequential()
+    model.add(Conv2D(16, (3, 3), padding = "same", kernel_regularizer=regularizers.l2(0.0001), activation='relu', input_shape=(32, 32, 1)))
+    model.add(Dropout(0.8))
+    model.add(MaxPooling2D(2,2))             
+    
+    model.add(Conv2D(16, (3, 3), padding = "same", kernel_regularizer=regularizers.l2(0.0001), activation='relu'))            
+    model.add(Dropout(0.2))
+    model.add(MaxPooling2D(2,2))
+    
+    model.add(Conv2D(16, (3, 3), padding = "same", kernel_regularizer=regularizers.l2(0.0001), activation='relu'))            
+    model.add(Dropout(0.2))
+    model.add(MaxPooling2D(2,2))
+    
+    model.add(Conv2D(16, (3, 3), padding = "same", kernel_regularizer=regularizers.l2(0.0001), activation='relu'))            
+    model.add(Dropout(0.2))
+    model.add(MaxPooling2D(2,2))
+    
+    model.add(Conv2D(16, (3, 3), padding = "same", kernel_regularizer=regularizers.l2(0.0001), activation='relu'))            
+    model.add(Dropout(0.2))
+    model.add(MaxPooling2D(2,2))
+        
+    model.add(Flatten())    
+    
+    model.add(Dense(64, activation='relu'))    
+    
+    model.add(Dense(num_classes, activation="softmax")) 
+    
+    model.compile(optimizer=tf.keras.optimizers.Adam(1e-3), loss='categorical_crossentropy',  metrics=['accuracy'])
+    
+    return model
+
+
+
+def get_callbacks():
+    
+                
+    tensorflow_run_folder = BASE_FOLDER + RUNS_FOLDER + MODEL_RUN_NAME
+    print("tensorflow_run_folder ", tensorflow_run_folder)
+    checkpoint_path = tensorflow_run_folder + "/training_1/cp.ckpt"
+    
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+    
+    log_dir = tensorflow_run_folder + "/logs/fit/" + run_sub_log_dir + "/"
+    
+    
+    tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1, profile_batch=0) 
+    
+    # https://machinelearningmastery.com/how-to-stop-training-deep-neural-networks-at-the-right-time-using-early-stopping/
+    # es_val_loss_callback = keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=30)
+    earlystop_train_loss_callback = keras.callbacks.EarlyStopping(monitor='loss', mode='min', verbose=1, patience=30)
+    
+    
+    
+    return [checkpoint_callback, earlystop_train_loss_callback,tensorboard_callback]
+
+    
+# def train_model(model, train_dataset, val_dataset):
+def train_model(model, train_x, train_y, val_x, val_y, ):
+    print("Training started")
+    
+
+#     https://keras.io/api/models/model_training_apis/
+    model.fit(x=train_x, y=train_y, 
+              validation_data=(val_x, val_y),
+              epochs=200, 
+              callbacks=get_callbacks(),
+              )
+    
+
+
+def evaluate_model(model, val_examples, val_labels):
+    print(model.evaluate(x=val_examples, y=val_labels))   
+  
+
+
+def prepare_data_no_dataset(create_data, testing, display_image):
+    # https://www.tensorflow.org/tutorials/load_data/numpy
+#     train_examples, val_examples, train_labels, val_labels, number_of_distinct_labels, sound_to_integer_mapping = prepare_data_v4.get_data(create_data=create_data, testing=testing, display_image=display_image)
+    train_examples, val_examples, train_labels, val_labels, number_of_distinct_labels, integer_to_sound_mapping = prepare_data_v4.get_data(create_data=create_data, testing=testing, display_image=display_image) 
+   
+   
+#     return train_examples, val_examples, train_labels, val_labels, number_of_distinct_labels, sound_to_integer_mapping
+    return train_examples, val_examples, train_labels, val_labels, number_of_distinct_labels, integer_to_sound_mapping
+
+
+# def create_integer_to_sound_mapping(sound_to_integer_mapping):
+# 
+#     print(sound_to_integer_mapping)
+#     integer_to_sound_mapping = {}
+#     for key, value in sound_to_integer_mapping.items():
+# #         print(item)
+#         integer_to_sound_mapping[value] = key
+#         
+#     print(integer_to_sound_mapping)
+#     return integer_to_sound_mapping
+
+def get_labels(unique_val_labels, sound_to_integer_mapping):
+    labels = []
+    for key, value in sound_to_integer_mapping.items():
+        labels.append(key)
+        
+    return labels
+
+
+          
+# def plot_confusion_matrix_3(predictions_decoded, val_labels_decoded, sound_to_integer_mapping):
+def plot_confusion_matrix_3(predictions_decoded, val_labels_decoded, integer_to_sound_mapping):
+    predictions_decoded_np = np.array(predictions_decoded)
+    val_labels_decoded_np = np.array(val_labels_decoded)
+    
+    unique_val_labels = np.unique(val_labels_decoded_np)
+    print(len(unique_val_labels))
+    
+#     lookup = create_integer_to_sound_mapping(sound_to_integer_mapping)
+    labels = []
+    for value in unique_val_labels:
+        sound = integer_to_sound_mapping.get(value)
+        labels.append(sound)
+        
+    
+    val_labels_decoded_names = []
+    predictions_decoded_names = []
+    
+#     labels = get_labels(unique_val_labels, sound_to_integer_mapping)
+    print(labels)
+   
+    from sklearn.metrics import confusion_matrix
+#     integer_to_sound_mapping = create_integer_to_sound_mapping(sound_to_integer_mapping)    
+    
+    for value in predictions_decoded_np:
+        predictions_decoded_names.append(integer_to_sound_mapping.get(value))
+        
+        
+    for value in val_labels_decoded_np:
+        val_labels_decoded_names.append(integer_to_sound_mapping.get(value))
+        
+        
+#     print(val_labels_decoded_names)
+    
+    
+    cm = confusion_matrix(val_labels_decoded_names, predictions_decoded_names)
+    # https://matplotlib.org/3.1.1/gallery/images_contours_and_fields/image_annotated_heatmap.html
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm)
+    
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(len(labels)))
+    ax.set_yticks(np.arange(len(labels)))
+    # ... and label them with the respective list entries
+    ax.set_xticklabels(labels)
+    ax.set_yticklabels(labels)
+    
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+    
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(labels)): 
+#     loop_size = len(labels) - 1
+#     for i in range(loop_size):        
+        for j in range(len(labels)): 
+#         for j in range(20):           
+            text = ax.text(j, i, cm[i, j], ha="center", va="center", color="w")
+    
+    ax.set_title("Confusion Matrix")
+    fig.tight_layout()
+    plt.show()
+
+
+def main():
+    create_data=False
+
+    testing=False # Only has an affect if create_data is True
+    
+    display_image = False # Only has an affect if create_data is True
+    
+    print("Started")
+        
+  
+    train_examples, val_examples, train_labels, val_labels, number_of_distinct_labels, sound_to_integer_mapping = prepare_data_no_dataset(create_data=create_data, testing=testing, display_image=display_image)
+    print("train_examples shape ", train_examples.shape)
+    print("train_labels shape ", train_labels.shape)    
+    print("This run used ", len(train_labels), " training examples")
+    
+    print("val_examples shape is ", val_examples.shape)
+    print("val_examples ndim is ", val_examples.ndim)
+    print("val_examples size is ", val_examples.size)
+    
+    # get one example
+    one_val_example = val_examples[:1]
+    print("one_val_example ", one_val_example)
+    
+    val_labels_decoded = tf.argmax(val_labels, 1)
+    print("val_labels_decoded ", val_labels_decoded)    
+
+    model = create_model_basic(number_of_distinct_labels)   
+
+    print(model.summary())
+
+    train_model(model, train_examples, train_labels, val_examples, val_labels)
+
+    print("val_examples shape is ", val_examples.shape)
+    print("val_examples ndim is ", val_examples.ndim)
+    print("val_examples size is ", val_examples.size)    
+ 
+    predictions = model(val_examples)
+    predictions_decoded = tf.argmax(predictions, 1)   
+
+    plot_confusion_matrix_3(predictions_decoded, val_labels_decoded, sound_to_integer_mapping)    
+
+    print("This run used ", len(train_labels), " training examples")
+    
+
+if __name__ == '__main__':
+    main()
